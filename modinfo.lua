f15b61573c629c69e590f79f74429e2cdd1bfa8e
GlyuKam <cherninartem@mail.ru>
2026-03-03 21:58:48 +0800

    added books: rain, bees, web, science and some quol code rewritings

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Tumbleweed tweaks"
 description = [[ RNG ЧИСТИЛИЩЕ]]
 author = "Тот Самый ГлюК"
-version = "1.097.8"
+version = "1.097.9"
 forumthread = ""
 dst_compatible = true
 dont_starve_compatible = false
--- reworks/prefabs/books.lua
+++ reworks/prefabs/books.lua
@@ -5,17 +5,18 @@ local books = {
     book_temperature           =   "point",
     book_fish                  =   "point",
     book_fire                  =   "point",
-    book_horticulture_upgraded =  "point",
-    -- book_web                   =   "point",
+    book_horticulture          =   "point",
+    book_horticulture_upgraded =   "point",
     book_birds                 =  "target",
-    -- book_horticulture          =  "target",
     book_silviculture          =  "target",
     book_light                 =  "target",
     book_light_upgraded        =  "target",
-    -- book_rain                  =  "target",
+    book_research_station      =  "target",
+    book_web                   =  "attack",
+    book_rain                  =  "attack",
     -- book_moon                  =  "target",
-    -- book_bees                  =  "target",
-    -- book_research_station      =  "target",
+    book_bees                  =  "attack",
+    
 }
 
 local armor = {
@@ -84,6 +85,32 @@ local moonitems = {
     "glasscutter",
 }
 
+local plants = {
+     "watermelon",
+         "tomato",
+        "pumpkin",
+         "potato",
+    "pomegranate",
+         "pepper",
+          "onion",
+         "garlic",
+       "eggplant",
+    "dragonfruit",
+           "corn",
+         "carrot",
+      "asparagus",
+}
+
+local spiders = {
+    "warrior",
+    "dropper",
+    "hider",
+    "spitter",
+    "moon",
+    "healer",
+    "water"
+}
+
 local function toall(inst)
     if inst.components.finiteuses then
         inst.components.finiteuses:Use(1)
@@ -115,6 +142,7 @@ local fns = {
             inst:Remove()
         end)
     end,
+
     book_brimstone = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         local inst = createinst(pos)
@@ -150,6 +178,7 @@ local fns = {
             toall(book)
         end)
     end,
+
     book_sleep = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         local inst = createinst(pos)
@@ -163,6 +192,7 @@ local fns = {
             inst:Remove()
         end)
     end,
+
     book_silviculture = function(book,target,pos)
         if not book:HasTag("canread") then return end
         book:DoTaskInTime(2,function() 
@@ -177,6 +207,7 @@ local fns = {
             toall(book)
         end)
     end,
+
     book_fish = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         local inst = createinst(pos)
@@ -210,12 +241,14 @@ local fns = {
 
         end)
     end,
+
     book_fire = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         local inst = createinst(pos)
 
         book:DoTaskInTime(2,function() 
             SpawnAt("fx_book_fire",book)
+            SpawnAt("stafflight",book)
             
             local task = nil
             task = inst:DoPeriodicTask(0.5,function()
@@ -239,6 +272,7 @@ local fns = {
 
         end)
     end,
+
     book_birds = function(book,target,pos)
         if not book:HasTag("canread") then return end
         local pos = Vector3(target.Transform:GetWorldPosition())
@@ -262,12 +296,14 @@ local fns = {
 
         end)
     end,
+
     book_temperature = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         local inst = createinst(pos)
 
         book:DoTaskInTime(2,function() 
             SpawnAt("fx_book_fire",book)
+            SpawnAt("staffcoldlight",book)
             
             local task = nil
             task = inst:DoPeriodicTask(0.5,function()
@@ -291,17 +327,47 @@ local fns = {
 
         end)
     end,
+
+    book_horticulture = function(book,hz,pos)
+        if not book:HasTag("canread") then return end
+        book:DoTaskInTime(2,function() 
+            local delta_theta = PI2 / 12
+            for i = 1, 12 do
+                local plant = SpawnPrefab("farm_plant_"..plants[math.random(#plants)])
+                if math.random(0,100)<95 then
+                    plant.components.growable:SetStage(5)
+                else
+                    plant.components.growable:SetStage(6)
+                end
+                if plant then
+                    plant.Transform:SetPosition(pos.x + 2.1 * math.cos( i*delta_theta ), 0, pos.z - 2.1 * math.sin( i*delta_theta ))
+                    local fx = SpawnAt("round_puff_fx_hi",plant)
+                end
+            end
+            toall(book)
+        end)
+    end,
+
     book_horticulture_upgraded = function(book,hz,pos)
         if not book:HasTag("canread") then return end
         book:DoTaskInTime(2,function() 
             local delta_theta = PI2 / 12
             for i = 1, 12 do
-                local durian = SpawnPrefab("durian_oversized")
-                durian.Transform:SetPosition(pos.x + 2.1 * math.cos( i*delta_theta ), 0, pos.z - 2.1 * math.sin( i*delta_theta ))
+                local plant
+                if math.random(0,100)<95 then
+                    plant = SpawnPrefab(plants[math.random(#plants)].."_oversized")
+                else
+                    plant = SpawnPrefab(plants[math.random(#plants)].."_oversized_rotten")
+                end
+                if plant then
+                    plant.Transform:SetPosition(pos.x + 2.1 * math.cos( i*delta_theta ), 0, pos.z - 2.1 * math.sin( i*delta_theta ))
+                    local fx = SpawnAt("round_puff_fx_hi",plant)
+                end
             end
             toall(book)
         end)
     end,
+
     book_light = function(book,target,pos)
         if not book:HasTag("canread") then return end
         book:DoTaskInTime(2,function() 
@@ -340,6 +406,102 @@ local fns = {
         end)
     end, 
 
+    book_bees = function(book,attacker,target)
+        if target.slowtask ~= nil then
+            target.slowtask:Cancel()
+        end
+        if target.canceltask ~= nil then
+            target.canceltask:Cancel()
+        end
+        target.slowtask = nil
+        target.canceltask = nil
+        target.slowtask = target:DoPeriodicTask(1,function()
+            local fx = SpawnAt("honey_trail",target)
+            fx:SetVariation(math.random(1,7),math.random(10,17)*0.1,3)
+        end)
+        target.canceltask = target:DoTaskInTime(5,function()
+            target.slowtask:Cancel()
+        end)
+        if target:HasTag("player") and not attacker:HasTag("noknockback") then
+            target.sg:GoToState("knockback")
+            target.Physics:SetMotorVel(20,0,0)
+            target:DoTaskInTime(0.5,function() target.Physics:Stop() end)
+            target.Transform:SetRotation(attacker.Transform:GetRotation())
+            attacker:AddTag("noknockback")
+            attacker:DoTaskInTime(5,function() attacker:RemoveTag("noknockback") end)
+        end
+    end,
+
+    book_web = function(book,attacker,target)
+        if target.spidertask ~= nil then
+            target.spidertask:Cancel()
+        end
+        if target.canceltask ~= nil then
+            target.canceltask:Cancel()
+        end
+        target.spidertask = nil
+        target.canceltask = nil
+        if target.components.inventory then
+            local hat = SpawnPrefab("spiderhat")
+            target.components.inventory:Equip(hat)
+        end
+        target.spidertask = target:DoPeriodicTask(0.5,function()
+            local pos = Vector3(target.Transform:GetWorldPosition())
+            local offset = FindWalkableOffset(pos,  math.random() * PI2, math.random(1,5), 8, false, true)
+            if offset then
+                local spider = SpawnPrefab("spider_"..spiders[math.random(#spiders)])
+                spider.Transform:SetPosition(pos.x+offset.x,0,pos.z+offset.z)
+                SpawnAt("round_puff_fx_hi",spider)
+            end    
+        end)
+        target.canceltask = target:DoTaskInTime(1.5,function()
+            target.spidertask:Cancel()
+        end)
+        if target:HasTag("player") and not attacker:HasTag("noknockback") then
+            target.sg:GoToState("knockback")
+            target.Physics:SetMotorVel(20,0,0)
+            target:DoTaskInTime(0.5,function() target.Physics:Stop() end)
+            target.Transform:SetRotation(attacker.Transform:GetRotation())
+            attacker:AddTag("noknockback")
+            attacker:DoTaskInTime(5,function() attacker:RemoveTag("noknockback") end)
+        end
+    end,
+
+    book_rain = function(book,attacker,target)
+        if target:HasTag("player") and not attacker:HasTag("noknockback") then
+            target.sg:GoToState("knockback")
+            target.Transform:SetRotation(attacker.Transform:GetRotation())
+            target.Physics:SetMotorVel(30,0,0)
+            RemovePhysicsColliders(target)
+            target:DoTaskInTime(0.5,function() target.Physics:Stop() ChangeToCharacterPhysics(target) end)
+            attacker:AddTag("noknockback")
+            attacker:DoTaskInTime(10,function() attacker:RemoveTag("noknockback") end)
+        end
+    end,
+
+    book_research_station = function(book,target,pos)
+        if not book:HasTag("canread") then return end
+        local pos = Vector3(target.Transform:GetWorldPosition())
+
+        book:DoTaskInTime(2,function() 
+            if target.components.inventoryitem then
+                if math.random(1,2)==2 then
+                    local pos = Vector3(target.Transform:GetWorldPosition())
+                    local offset = FindWalkableOffset(pos,  math.random() * PI2, math.random(1,5), 3, false, true)
+                    if offset then
+                        local double = SpawnPrefab(target.prefab)
+                        double.Transform:SetPosition(pos.x+offset.x,0,pos.z+offset.z)
+                        SpawnAt("round_puff_fx_hi",double)
+                    end   
+                else
+                    SpawnAt("round_puff_fx_hi",target)
+                    target:Remove()
+                end
+                toall(book)
+            end
+        end)
+    end,
+
 }
 
 local function anim(inst,target,caster)
@@ -358,32 +520,56 @@ end
 for book,value in pairs(books) do 
     AddPrefabPostInit(book,function(inst)
         inst:RemoveComponent("book")
-
-        local spellcaster = inst:AddComponent("spellcaster")
+        local spellcaster
+        if value~="attack" then
+            spellcaster = inst:AddComponent("spellcaster")
+        end
         if not TheWorld.ismastersim then return end
         inst:AddComponent("equippable")
 
         local weapon = inst:AddComponent("weapon")
         weapon.damage = 25
-        weapon.attackrange = 1
-        weapon.hitrange = 1
+        weapon.attackrange = 0.5
+        weapon.hitrange = 0.5
+        weapon:SetOnAttack(function(inst,attacker,target)
+            if target:HasTag("player") and not attacker:HasTag("noknockback") then
+                target.sg:GoToState("knockback")
+                target.Physics:SetMotorVel(20,0,0)
+                target:DoTaskInTime(0.5,function() target.Physics:Stop() end)
+                target.Transform:SetRotation(attacker.Transform:GetRotation())
+                attacker:AddTag("noknockback")
+                attacker:DoTaskInTime(5,function() attacker:RemoveTag("noknockback") end)
+            end
+        end)
 
-        spellcaster.quickcast = true
-        spellcaster:SetOnSpellCastFn(anim)
+
         if value == "target" then
-            spellcaster.canuseontargets = true
-            spellcaster.canuseonlocomotors = true
-            spellcaster.canuseondead = false
-            spellcaster.canuseonpoint = false
+            spellcaster.quickcast = true
+            spellcaster:SetOnSpellCastFn(anim)
+            if book~="book_research_station" then
+                spellcaster.canuseontargets = true
+                spellcaster.canuseonlocomotors = true
+                spellcaster.canuseondead = false
+                spellcaster.canuseonpoint = false
 
-            spellcaster.canonlyuseonlocomotors = true
-            spellcaster.canonlyuseonlocomotorspvp = true
-            
+                spellcaster.canonlyuseonlocomotors = true
+                spellcaster.canonlyuseonlocomotorspvp = true
+            else
+                spellcaster.canuseontargets = true
+                spellcaster.canuseonlocomotors = false
+                spellcaster.canuseondead = false
+                spellcaster.canuseonpoint = false
+                spellcaster.canonlyuseonrecipes = true
+            end
             spellcaster:SetSpellFn(fns[book])
         elseif value == "point" then
+            spellcaster.quickcast = true
+            spellcaster:SetOnSpellCastFn(anim)
             spellcaster.canuseonpoint = true
             
             spellcaster:SetSpellFn(fns[book])
+        elseif value == "attack" then
+            weapon:SetOnAttack(fns[book])
         end
 
         inst.components.finiteuses:SetUses(3)
